8de4648c89754e38b13117064902b447ea22ecb9
Dennis Littlefield <Littlefield.Dennis@outlook.com>
2023-12-22 12:02:06 -0500

    authentication added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,14 @@ export default function Home() {
     <>
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-          <p className="text-sm font-semibold text-gray-700">PDF Document Chat</p>
+          <p className="text-sm font-semibold text-gray-700">Collective Voices</p>
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-          Chat with your <span className="text-blue-600">documents</span> in seconds.
+          Chat with your <span className="text-blue-600">Collective Agreement</span> in seconds.
         </h1>
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-          Do you have a large document you need to summarize or reference. Simply upload a PDF and start asking questions.
+
+          Engage with your Collective Agreement swiftly. Whether you have an extensive document requiring summarization or reference, just upload the PDF and begin your inquiries.
         </p>
         <Link className={buttonVariants({
           size: 'lg',
